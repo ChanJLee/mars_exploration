0b8f8dd8f200b2d089a628a82304c0ea6962c4b5
ChanJLee <1355425625@qq.com>
2017-12-16 23:54:36 +0800

    sync

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -9,3 +9,12 @@ Java_com_chan_mars_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_chan_mars_MainActivity_read(JNIEnv *env, jobject instance, jbyteArray data_, jint len) {
+    jbyte *data = env->GetByteArrayElements(data_, NULL);
+
+    // TODO
+
+    env->ReleaseByteArrayElements(data_, data, 0);
+}
